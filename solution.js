ddb0b054c1407cdf6163741c184be78539a26313
Rami B <ramibasheer9@gmail.com>
2022-07-05 14:10:56 +0200

    submit solutions

---
+++ solution.js
@@ -0,0 +1,26 @@
+// 1.
+console.log(Math.min(-1, 4));
+
+console.log(Math.max(-1, 4));
+
+// 2.
+
+console.log(Math.round(3321.32321));
+console.log(Math.round(326.76));
+console.log(Math.round(76788.7));
+console.log(Math.round(-9.78));
+console.log(Math.round(43.342));
+
+console.log('----------------');
+
+console.log(Math.floor(3321.32321));
+console.log(Math.floor(326.76));
+console.log(Math.floor(76788.7));
+console.log(Math.floor(-9.78));
+console.log(Math.floor(28.329));
+
+// 3.
+
+const shuffel = ['1', '2', '3', '4', '5', '6']
+console.log(Math.random());
+console.log(shuffel[Math.floor( Math.random() * 10)]);
